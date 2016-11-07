60fc674ba7da47db9526554b0f856cc6b2e4ee47
petrokulbida <petr.kylbida@gmail.com>
2016-11-07 16:52:15 +0200

    IGallery v.0.2

--- src/ImageGallery/ImageGalleryBundle/Controller/DefaultController.php
+++ src/ImageGallery/ImageGalleryBundle/Controller/DefaultController.php
@@ -10,4 +10,9 @@ class DefaultController extends Controller
     {
         return $this->render('PetroImageGalleryBundle:Default:index.html.twig');
     }
+
+    public function aboutAction()
+    {
+        return $this->render('PetroImageGalleryBundle:Default:about.html.twig');
+    }
 }
